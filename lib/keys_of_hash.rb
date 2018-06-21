955ff8f9aff9c1e6470843c0bfe7e7e101ab757a
Yicheng Jin <bobjin42@gmail.com>
2018-06-21 16:50:11 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,7 @@
 class Hash
   def keys_of(*arguments)
     new_arr = []
-      keys.each{|key, value|
+      self.each{|key, value|
       if arguments.include?value
          new_arr << key
       end
